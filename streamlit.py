1a89e587e98e7f009a8b62e5844639f5c905f732
Griffin Barich <gbarich@wesleyan.edu>
2025-03-15 12:31:16 -0400

    add Dynamic filtering

--- streamlit.py
+++ streamlit.py
@@ -1,18 +1,23 @@
-import streamlit as st
 import pandas as pd
-from src.classes.cast_generator import CastGenerator
-    
+from streamlit_dynamic_filters import DynamicFilters
 
-st.title("All Casts")
-roles_file = st.file_uploader('Upload Roles', type='csv')
-preferences_file = st.file_uploader('Upload Preferences', type='csv')
+import streamlit as st
+from src.classes.cast_generator import CastGenerator
+
+st.title("Get All Rocky Horror Casts")
+roles_file = st.file_uploader("Upload Roles", type="csv")
+preferences_file = st.file_uploader("Upload Preferences", type="csv")
 
 if roles_file is not None and preferences_file is not None:
     roles = pd.read_csv(roles_file)
-    preferences =pd.read_csv(preferences_file)
+    preferences = pd.read_csv(preferences_file)
     available_members = preferences["member"].to_list()
-    df = CastGenerator(
-    available_members=available_members, roles=roles, preferences=preferences
-    ).get_all_casts()
-    
-    st.write(df)
+    cg = CastGenerator(available_members=available_members, roles=roles, preferences=preferences)
+    df = cg.get_all_casts()
+
+    filters = DynamicFilters(df, filters=cg.roles_list)
+
+    with st.sidebar:
+        filters.display_filters()
+
+    filters.display_df()
